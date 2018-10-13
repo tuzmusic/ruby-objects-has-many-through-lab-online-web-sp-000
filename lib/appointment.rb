47371b3c83f49db0669ed526cb8c34e485e56eee
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-13 22:55:10 +0000

    All tests passing

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+
+require "pry"
+class Appointment
+
+  attr_accessor :patient, :doctor, :date
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date
+    @@all << self
+    # binding.pry
+  end
+end
